b56d4c373ad39992f7399e03535531267ddd3e26
b-shelton <49220496+b-shelton@users.noreply.github.com>
2019-04-06 16:12:05 -0700

    Update supervised_model.R

--- machine_learning/supervised_model/supervised_model.R
+++ machine_learning/supervised_model/supervised_model.R
@@ -45,7 +45,7 @@ set.seed(32541)
 Sys.setenv("AWS_ACCESS_KEY_ID" = "AKIAUL755PNFVNWYTNP7",
            "AWS_SECRET_ACCESS_KEY" = "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",
            "AWS_DEFAULT_REGION" = "us-east-2")
-loans1 <- df <- aws.s3::s3read_using(FUN = fread, object = "loan_data.csv", bucket = "example.data")
+loans1 <- aws.s3::s3read_using(FUN = fread, object = "loan_data.csv", bucket = "example.data")
 
 
 ##################################################################################################
